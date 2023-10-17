681a6383f6c57674a66193d15d3fee8a78e9c6ff
Suraj Sutar <69454337+Tech-Surya@users.noreply.github.com>
2023-10-17 08:59:52 +0530

    Create math_cmp.cpp

---
+++ math_cmp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    cout << isgreater(48,47)<< "\n";
+    cout << islessequal(11,5)<< "\n";
+    cout << isgreaterequal(19,72)<< "\n";
+    cout << islessgreater(59,84)<< "\n";
+    cout << isunordered(62,84)<< "\n";
+    return 0;
+}
