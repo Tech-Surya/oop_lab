e19c4d316ad554d4b61c457abfcfb1a49b099732
Suraj Sutar <69454337+Tech-Surya@users.noreply.github.com>
2023-10-17 08:56:59 +0530

    Create math_pow.cpp

---
+++ math_pow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    double c;
+    cout<<"Enter two integer numbers\n";
+    cin>>a>>b;
+    c=pow(a,b);
+    cout<<"Power = "<<c;
+    return 0;
+}
